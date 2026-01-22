0f27f316164ee4455b74db77a942ace3f8e7f570
Christian Rivera <christianrivera488@gmail.com>
2026-01-22 01:27:51 -0500

    fixed up front end and added more comments for learning purposes

--- public/main.js
+++ public/main.js
@@ -2,38 +2,45 @@ document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById('brawler-container');
     
     document.getElementById("fetchdata").addEventListener("click", () => {
-    const tag = document.getElementById("supercell").value; //end it with .value
-    const encodedTag = encodeURIComponent(tag);
     
-    if (!tag){
-        alert("ENTER TAG");
-    }
-
-    fetch(`http://localhost:3000/player/${encodedTag}`)
-        .then(res => res.json())
-        .then(data => {
-            if (!data || data.reason){
-                console.error("API Erorr: ", data);
-                document.getElementById('data-container').innerHTML = data.message || "Error fetching data"
-                container.innerHTML = "";
-                return;
-            }
-            document.getElementById('data-container').innerHTML = `${data.name}`
-            if (Array.isArray(data.brawlers)){
-                data.brawlers.forEach(brawler => {
-                    console.log(`${brawler.name}`);
-                    const index = document.createElement('li'); //we create an element here to insert or rather create of for data
-                    index.textContent = `${brawler.name}`;
-                    container.appendChild(index); //attach new element to another element in the page
-                    //child means inside another element, parent is the element title aka index
-                })
-            }
-            else {
-                console.error('Brawlers not found', data);
-            }
-        })
-        .catch(error => console.error(error));
+        const tag = document.getElementById("supercell").value; //end it with .value
+        const encodedTag = encodeURIComponent(tag);
+    
+    
+        if (!tag){
+            alert("ENTER TAG");
+        }
+        //user enters tag in input field, we grab it
+        //button click activates, fetch request is sent
+        //send a request to the server, through background
+        //server recieves it at, app.get, then uses api key
+        //
+    
+        fetch(`http://localhost:3000/player/${encodedTag}`) //browser ask server for player data
+            .then(res => res.json()) //does not use await because code runs on click event, recieves the data here from server
+            .then(data => { //uses "then" instead of await 
+                if (!data || data.reason){
+                    console.error("API Erorr: ", data);
+                    document.getElementById('data-container').innerHTML = data.message || "Error fetching data"
+                    container.innerHTML = "";
+                    return;
+                }
+                document.getElementById('data-container').innerHTML = `${data.name}`
+                if (Array.isArray(data.brawlers)){
+                    data.brawlers.forEach(brawler => {
+                        console.log(`${brawler.name}`);
+                        const index = document.createElement('li'); //we create an element here to insert or rather create of for data
+                        index.textContent = `${brawler.name}`;
+                        container.appendChild(index); //attach new element to another element in the page
+                        //child means inside another element, parent is the element title aka index
+                    })
+                }
+                else {
+                    console.error('Brawlers not found', data);
+                }
+            })
+            .catch(error => console.error(error));
+        });
     });
-});
 
 
--- server.js
+++ server.js
@@ -1,32 +1,38 @@
-require('dotenv').config();
-
+require('dotenv').config(); //package that helps you manage environment variables
 const express = require("express");
-const fetch = require("node-fetch");
 const cors = require("cors");
-const app = express();
+const app = express(); //imports the express framework
 //brawl stars test id = #209P09L8
 
 const PORT = 3000;
 
-app.use(cors());
-app.use(express.static("public"));
+app.use(cors()); //cors is cross origin resource sharing, that allows us to make requests from different domains
+app.use(express.static("public")); //tells express to serever all html,css,js files in public fodler
+//app.use is a method that sets up middle ware, their like instructions
 const API_KEY = process.env.API_KEY; //safe
 if (!API_KEY) {
     console.error("ERROR: API_KEY not found in .env file!");
 }
 
+//get is for retreiving data, post would be for sending
 app.get("/player/:tag", async (req, res) => {
-    const playerTag = encodeURIComponent(req.params.tag);
-    try{
-        const response = await fetch(`https://api.brawlstars.com/v1/players/${playerTag}`, {
+    const playerTag = encodeURIComponent(req.params.tag); //grabs the value from the url and formats for API
+    try{ //the ":" tells express that this part changes based on user
+        const response = await fetch(`https://api.brawlstars.com/v1/players/${playerTag}`, { //await is so it doesnt activiate without fetching first
             headers: {"Authorization": `Bearer ${API_KEY}`} //Bearer std way of sending API auntentication 
         });
         const data = await response.json();
         res.json(data); //response json is a method on the fetch response not express
+        //sends data back to browser
     }
     catch (error){
         res.status(500).json({ error : error.message });
     }
+});
+
+const path = require("path");
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "dashboard.html")); //serve static files
 });
  
 app.listen(PORT, () => console.log(`Server running at on port: ${PORT}`)); //to know which port i am
